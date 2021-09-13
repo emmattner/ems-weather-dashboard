5b7ce1acf898107f2922644f049bccb219b1b4bb
Emma Mattner <emattner1997@gmail.com>
2021-09-13 12:37:05 +0930

    js declarations

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const API_Key = "4a3134ff84b520e99655fd55fab2c3b8"
+
+const cityArray = [''];
+const searchEl = document.querySelector("#search-container");
+const cityInputEl = document.querySelector("#city-input");
+const cityHistoryEl = document.querySelector("#search-history");
+const citySearchInputEl = document.querySelector("#searched-city");
+const currentWeatherEl =document.querySelector("#current-weather-container");
+const forecastHeader = document.querySelector("#forecast-header");
+const forecastContainerEl = document.querySelector("#forecast-container");
